36806be01be59f20a0fe872386e516933144dc7b
zaky <zakiulla88@gmail.com>
2025-01-17 17:39:56 +0530

    Websocket Setup with uvicorn and ngrok to get it online

--- server/AES_Glove/asgi.py
+++ server/AES_Glove/asgi.py
@@ -10,7 +10,18 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from AES_Glove.websocket import websocket_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AES_Glove.settings')
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+
+async def application(scope, receive, send):
+    if scope['type'] == 'http':
+        # Handle HTTP requests with the default Django application
+        await django_application(scope, receive, send)
+    elif scope['type'] == 'websocket':
+        # Handle WebSocket connections with your custom logic
+        await websocket_application(scope, receive, send)
+    else:
+        raise NotImplementedError(f"Unknown scope type {scope['type']}")
