605917ec7867a2ec5c1e19974f2b08137e994c73
Muaaz EGJM <muznymuaaz22@gmail.com>
2025-08-19 22:32:58 +0530

    Update flexClassifier.js

--- client/src/utils/flexClassifier.js
+++ client/src/utils/flexClassifier.js
@@ -1,44 +1,44 @@
 // Flex classification utility
-
+//"EF_Flex", "IF_Flex", "MF_Flex", "PF_Flex", "RF_Flex", "TF_Flex", "WF_Flex"
 export const classifyFlexValue01 = (value) => {
-  if (value < 25) return "No Bend";
-  if (value < 50) return "Almost No Bend";
-  if (value < 75) return "Almost Full Bend";
+  if (value < 5000) return "No Bend";
+  if (value < 3200) return "Almost No Bend";
+  if (value < 1400) return "Almost Full Bend";
   return "Full Bend";
 };
 export const classifyFlexValue02 = (value) => {
-  if (value < 20) return "No Bend";
-  if (value < 50) return  "Almost No Bend";
-  if (value < 70) return "Almost Full Bend";
+  if (value < 5000) return "No Bend";
+  if (value < 3200) return  "Almost No Bend";
+  if (value < 1400) return "Almost Full Bend";
   return "Full Bend";
 };
 export const classifyFlexValue03 = (value) => {
-  if (value < 35) return "No Bend";
-  if (value < 70) return  "Almost No Bend";
-  if (value < 85) return "Almost Full Bend";
+  if (value < 5000) return "No Bend";
+  if (value < 3200) return  "Almost No Bend";
+  if (value < 1400) return "Almost Full Bend";
   return "Full Bend";
 };
 export const classifyFlexValue04 = (value) => {
-  if (value < 40) return "No Bend";
-  if (value < 70) return  "Almost No Bend";
-  if (value < 90) return "Almost Full Bend";
+  if (value < 5000) return "No Bend";
+  if (value < 3200) return  "Almost No Bend";
+  if (value < 1400) return "Almost Full Bend";
   return "Full Bend";
 };
 export const classifyFlexValue05 = (value) => {
-  if (value < 30) return "No Bend";
-  if (value < 60) return  "Almost No Bend";
-  if (value < 85) return "Almost Full Bend";
+  if (value < 5000) return "No Bend";
+  if (value < 3200) return  "Almost No Bend";
+  if (value < 1400) return "Almost Full Bend";
   return "Full Bend";
 };
 export const classifyFlexValue06 = (value) => {
-  if (value < 35) return "No Bend";
-  if (value < 65) return  "Almost No Bend";
-  if (value < 90) return "Almost Full Bend";
+  if (value < 5000) return "No Bend";
+  if (value < 3200) return  "Almost No Bend";
+  if (value < 1400) return "Almost Full Bend";
   return "Full Bend";
 };
 export const classifyFlexValue07 = (value) => {
-  if (value < 40) return "No Bend";
-  if (value < 70) return  "Almost No Bend";
-  if (value < 95) return "Almost Full Bend";
+  if (value < 5000) return "No Bend";
+  if (value < 3200) return  "Almost No Bend";
+  if (value < 1400) return "Almost Full Bend";
   return "Full Bend";
 };
